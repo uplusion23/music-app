f6ac4a5d5c5d4e7b5a37b814042945cbaadf4ac3
Trevor McIntire <uplusion23@gmail.com>
2018-08-11 23:30:46 +0900

    Add more menu toggles Show username in sidemenu header

--- bin/assets/js/display.js
+++ bin/assets/js/display.js
@@ -1,3 +1,15 @@
+var disGlobals = {
+  username: require("os").userInfo().username
+}
+
 $('body').on('click', '.menuToggle', function() {
-  $('body').toggleClass("sidemenuActive");
+  $('body').addClass("sidemenuActive");
+});
+
+$('body').on('click', '.shadow', function() {
+  $('body').removeClass("sidemenuActive");
+});
+
+$(document).ready(function() {
+  $('.sidemenu > .header').text(disGlobals.username)
 });
