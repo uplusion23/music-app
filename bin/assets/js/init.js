a4d6f8f3d08c6776f9c6f6fe69082648d7e511d3
Killinglands <glasser1198@gmail.com>
2018-08-12 13:52:58 +0900

    login Jquery

--- bin/assets/js/init.js
+++ bin/assets/js/init.js
@@ -1,27 +1,41 @@
-var tracklist = document.querySelectorAll('.track');
+$(document).ready(function(){
+$('#login').click(function(){
 
-document.addEventListener('keypress', function(key){
-  if(key.keyCode == 32){
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  if(email=='' || password == '')
+  {
+    $('input[type="text"],input[type="password"]').css("boarder","2px solid red");
+    $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
+    alert("Please fill all fields!");
   }
+  else{
+    $.post("login.php",{ email: email, password1: password},
+    function(data)
+    {
+      if(data=='Invalid Email')
+      {
+        $('input[type="text"]').css({"boarder":"2px solid red", "box-shadow":"0 0 3px red"});
+        $('input[type="password"]').css({"boarder":"2px solid #00F5FF", "box-shadow":"0 0 5px #00F5FF"});
+        alert(data);
+      }
+      else if(data=='Email or Password is wrong!')
+      {
+        $('input[type="text"]').css({"boarder":"2px solid red", "box-shadow":"0 0 3px red"});
+      }
+      else if(data=='sucessfully Logged in')
+      {
+        $('input[type="text"], input[type="password"]').css({"boarder":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
+        alert(data);
+      }
+      else
+      {
+        alert(data);
+      }
+    });
+  }
+
+}
 });
-
-function playpause(){
-  tracklist[i].addEventListener('click', function(e){
-
-  });
-}
-
-for(i=0; i < trackList.length; i++){
-  playpause();
-}
-
-function findIndex(el){
-  var i=1;
-  while(el.previousSibling){
-    el = el.previousSibling;
-    if(el.nodeType === 1){
-      i++;
-    }
-  }
-  return i;
-}
+});
